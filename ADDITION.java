a290a559d3484d81904fa32b0135509c4fe546e3
Carmelo Banias <110279674+Carms12@users.noreply.github.com>
2025-05-08 00:50:17 +0800

    Create ADDITION.java

---
+++ ADDITION.java
@@ -0,0 +1,13 @@
+public class Addition {
+    public static void main(String[] args) {
+        // Declare two numbers
+        int num1 = 5;
+        int num2 = 7;
+        
+        // Perform the addition
+        int sum = num1 + num2;
+        
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
