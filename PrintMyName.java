1cac7f134592d8a3ed2aa5a493ae94c164ca8b99
Carmelo Banias <110279674+Carms12@users.noreply.github.com>
2025-05-08 07:06:36 +0800

    Create PrintMyName.java

---
+++ PrintMyName.java
@@ -0,0 +1,7 @@
+public class PrintMyName {
+    public static void main(String[] args) {
+        String firstName = "Carmelo";
+        String lastName = "Banias";
+        System.out.println("My name is " + firstName + " " + lastName + ".");
+    }
+}
